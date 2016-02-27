a5a79bc925b2dc9fa75a430fe19157e84c7db5e3
ariel anders <anders.ariel@gmail.com>
2016-02-27 10:15:40 -0500

    basic stuff ok

--- boop/ViewController.swift
+++ boop/ViewController.swift
@@ -7,12 +7,75 @@
 //
 
 import UIKit
+import AudioToolbox
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hi: UIButton!
+    var mySound: AVAudioPlayer?
+    @IBOutlet weak var capturedImage: UIImageView!
+    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var capturePhoto: UIButton!
+    var captureSession: AVCaptureSession?
+    var stillImageOutput: AVCaptureStillImageOutput?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // initialize the sound
+        if let sound = self.setupAudioPlayerWithFile("yourAudioFileName", type: "mp3") {
+            self.mySound = sound
+        }
+        NSLog("Hello world! Loaded Program!")
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession!.sessionPreset = AVCaptureSessionPresetPhoto
+        
+        let backCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        var error: NSError?
+        var input: AVCaptureDeviceInput!
+        do {
+            input = try AVCaptureDeviceInput(device: backCamera)
+        } catch let error1 as NSError {
+            error = error1
+            input = nil
+        }
+        
+        if error == nil && captureSession!.canAddInput(input) {
+            captureSession!.addInput(input)
+            
+            stillImageOutput = AVCaptureStillImageOutput()
+            stillImageOutput!.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
+            if captureSession!.canAddOutput(stillImageOutput) {
+                captureSession!.addOutput(stillImageOutput)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                previewLayer!.videoGravity = AVLayerVideoGravityResizeAspect
+                previewLayer!.connection?.videoOrientation = AVCaptureVideoOrientation.Portrait
+                previewView.layer.addSublayer(previewLayer!)
+                
+                captureSession!.startRunning()
+            }
+        }
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer!.frame = previewView.bounds
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +83,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hi(sender: AnyObject) {
+        
+        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+        mySound?.play() // ignored if nil
+    }
 
+    @IBAction func didPressTakePhoto(sender: UIButton) {
+        
+        if let videoConnection = stillImageOutput!.connectionWithMediaType(AVMediaTypeVideo) {
+            videoConnection.videoOrientation = AVCaptureVideoOrientation.Portrait
+            stillImageOutput?.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: {(sampleBuffer, error) in
+                if (sampleBuffer != nil) {
+                    let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+                    let dataProvider = CGDataProviderCreateWithCFData(imageData)
+                    let cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true, CGColorRenderingIntent.RenderingIntentDefault)
+                    
+                    let image = UIImage(CGImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.Right)
+                    self.capturedImage.image = image
+                    
+                    NSLog("width:%d", CGImageGetWidth(cgImageRef))
+                    NSLog("height:%d", CGImageGetWidth(cgImageRef))
+
+                    
+                    
+                }
+            })
+        }
+    }
+    
+    @IBAction func didPressTakeAnother(sender: AnyObject) {
+        captureSession!.startRunning()
+    }
+
+    
+    
+    func setupAudioPlayerWithFile(file: NSString, type: NSString) -> AVAudioPlayer? {
+        
+        let path = NSBundle.mainBundle().pathForResource(file as String, ofType: type as String)
+        let url = NSURL.fileURLWithPath(path!)
+        var audioPlayer: AVAudioPlayer?
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: url)
+        } catch {
+            print("Player not available")
+        }
+        
+        return audioPlayer
+    }
 }
-
